4c0a41edfab0086be71b179aa4d1eea881fb4b65
coder7475 <robiulhossain7475@gmail.com>
2025-11-26 23:02:59 +0600

    feat: create first agent

--- 1_single_agent/main.py
+++ 1_single_agent/main.py
@@ -2,12 +2,32 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from dotenv import load_dotenv
+from agent.agent import root_agent
+from google.adk.runners import InMemoryRunner
 
-from config import get_settings
-settings = get_settings()
 
-def main():
-    print(settings.google_api_key)
+load_dotenv()  
+import asyncio
+
+async def call_llm(runner, query):
+    return await runner.run_debug(
+        query
+    )
+
+async def main():
+    # google_api_key = os.getenv("GOOGLE_API_KEY")
+    # print("Google API Key:", google_api_key)
+    # print(root_agent)
+    runner = InMemoryRunner(agent=root_agent)
+
+    print("✅ Runner created.")
+
+    query = "What is the time in Chittagong?"
+    response = await call_llm(runner, query)
+
+    print(response)
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
